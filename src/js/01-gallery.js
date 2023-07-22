42e1726fbdb61dbbd4e794219393978a2211bc6e
odi2n1 <123581559+odi2n1@users.noreply.github.com>
2023-07-22 19:42:18 +0300

    commi

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,6 +6,7 @@ console.log(galleryItems);
 
 import SimpleLightbox from 'simplelightbox';
 
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
